120bc9f836138e19af20541a4b4a67c286f30456
Ali Umar <ali.umar.work@gmail.com>
2023-04-29 23:48:47 -0400

    udate search

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,7 +1,6 @@
 -- Telescope mappings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('i', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 -- NERDTree
 vim.api.nvim_set_keymap('n', '<C-t>', ":NERDTreeToggle<CR>", { noremap = true });
 vim.api.nvim_set_keymap('i', '<C-t>', ":NERDTreeToggle<CR>", { noremap = true });
